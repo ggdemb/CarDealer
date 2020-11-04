f4208880b9c1913e2fef58c8667949962e19a5d7
Grzegorz Dembowski <ggdemb@gmail.com>
2020-11-04 08:44:59 +0100

    FodyWeaver NullCheck and Maybe struct as substitution for nulls in domain core

--- CarDealer.Sale.Domain/DependencyInjectionRegistrant.cs
+++ CarDealer.Sale.Domain/DependencyInjectionRegistrant.cs
@@ -1,7 +1,9 @@
 ﻿using CarDealer.Domain.Common;
 using CarDealer.Domain.SharedKernel;
 using Microsoft.Extensions.DependencyInjection;
+using NullGuard;
 
+[assembly: NullGuard(ValidationFlags.All)]
 namespace CarDealer.Sale.Domain
 {
     public static class DependencyInjectionRegistrant
