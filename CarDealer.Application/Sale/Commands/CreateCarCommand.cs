13048a041392ce3db0a2778bcff020e6c6c6d0c1
Grzegorz Dembowski <ggdemb@gmail.com>
2020-11-07 10:33:05 +0100

    Created car creation command with MediatR

---
+++ CarDealer.Application/Sale/Commands/CreateCarCommand.cs
@@ -0,0 +1,10 @@
+﻿using CarDealer.Domain.Common;
+using MediatR;
+
+namespace CarDealer.Application.Sale.Commands
+{
+    public class CreateCarCommand : IRequest<Result>
+    {
+        public int SampleProp { get; set; }
+    }
+}
---
+++ CarDealer.Application/Sale/Commands/CreateCarCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using CarDealer.Application.CommonContracts;
+using CarDealer.Domain.Common;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CarDealer.Application.Sale.Commands
+{
+    public class CreateCarCommandHandler : IRequestHandler<CreateCarCommand, Result>
+    {
+        private readonly ICarDealerContext _dbContext;
+
+        public CreateCarCommandHandler(ICarDealerContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task<Result> Handle(CreateCarCommand request, CancellationToken cancellationToken)
+        {
+            //TODO: here you can orchestrate domain actions
+            return await Task.FromResult(Result.Ok());
+        }
+    }
+}
