b1bbd6b904e3a1ae482c3a99b3bdc18fbd32b4a1
Grzegorz Dembowski <ggdemb@gmail.com>
2020-11-08 20:33:33 +0100

    Conversion between application and domain enums

---
+++ CarDealer.Application/Sale/Common/SaleApplicationEnums.cs
@@ -0,0 +1,34 @@
+﻿using CarDealer.Application.Utils;
+using CarDealer.Domain.Sale.Car;
+using System;
+
+namespace CarDealer.Application.Sale.Common
+{
+    [CustomSerializationName("TransmissionType")]
+    public enum CommandTransmissionType
+    {
+        Manual,
+        Automatic
+    }
+    [CustomSerializationName("CarType")]
+    public enum CommandCarType
+    {
+        Sport,
+        Regular
+
+    }
+    [CustomSerializationName("CarState")]
+    public enum CommandCarState
+    {
+        New,
+        Used,
+        Broken
+    }
+    static class ApplicationEnumsExtensions
+    {
+        public static TResult Transform<TSource, TResult>(this TSource inputEnum) where TSource : struct where TResult : struct
+        {
+            return Enum.Parse<TResult>(inputEnum.ToString());
+        }
+    }
+}
